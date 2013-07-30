885f053a8eaf9d94fe4bd771054f1a1335d6c108
Tejaswita <tejaswita@Tejaswita.(none)>
2013-07-30 17:32:57 +0530

    adding NameResource

--- src/main/java/resource/NameResource.java
+++ src/main/java/resource/NameResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,6 +17,7 @@ public class NameResource {
 	@GET
 	@Path("/{name}")
 	@Consumes(MediaType.TEXT_HTML)
+	@Produces(MediaType.TEXT_HTML)
 	public String display(@PathParam("name") String name) {
 		if (StringUtils.isBlank(name)) {
 			Response response = Response.status(Response.Status.BAD_REQUEST)
